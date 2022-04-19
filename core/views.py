d5cc57976a98744b0a762b8635612d0fcbc55914
montalvas <lucas_montalvani@hotmail.com>
2022-04-19 19:41:58 -0300

    config urls, views and templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+rooms = [
+    {'id': 1, 'name': 'Python'},
+    {'id': 2, 'name': 'Javascript'},
+    {'id': 3, 'name': 'Kotlin'}
+]
+
+def home(request):
+    return render(request, 'core/home.html', {'rooms': rooms})
+
+def room(request, pk):
+    room = None
+    
+    for r in rooms:
+        if r['id'] == int(pk):
+            room = r
+    context = {'room': room}
+    
+    return render(request, 'core/room.html', context)
