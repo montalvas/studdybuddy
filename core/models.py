4ee957cbbf091aac09476f2a9070b003825b60a4
montalvas <lucas_montalvani@hotmail.com>
2022-04-23 10:09:14 -0300

    CRUD in function views

--- core/models.py
+++ core/models.py
@@ -17,6 +17,9 @@ class Room(models.Model):
     # participants
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.name
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -11,3 +12,44 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     
     return render(request, 'core/room.html', {'room': room})
+
+def create_room(request):
+    form = RoomForm()
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('core:home')
+    
+    context = {
+        'form': form,
+        'title': 'Create Room'
+        }
+    
+    return render(request, 'core/form_room.html', context)
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('core:home')
+    
+    context = {
+        'form': form,
+        'title': 'Update Room'
+        }
+    
+    return render(request, 'core/form_room.html', context)
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('core:home')
+    
+    return render(request, 'core/delete_room.html', {'obj': room})
